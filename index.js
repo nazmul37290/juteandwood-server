de9d8101a1a26b97c4bf3baed6a7548911b2dca2
nazmul37290 <nazmulnjm@gmail.com>
2024-04-26 14:49:43 +0600

    connected to mongodb succesfully

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ app.get("/", (req, res) => {
   res.send("Assignment server is running");
 });
 
+// AacWizo72QLDQdtx
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://nazmulnjm:AacWizo72QLDQdtx@cluster0.0kf8y7n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
